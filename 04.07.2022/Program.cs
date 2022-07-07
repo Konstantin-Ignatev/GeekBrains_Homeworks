31d0d47c67decff30dc6dfe49b2e4582f5b0e6cf
Konstantin Ignatev <ignatyev.k@yandex.ru>
2022-07-07 17:02:12 +0300

    Task_27_added

--- 04.07.2022/Program.cs
+++ 04.07.2022/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int FindSum(int i)
+{
+    int sum = 0;
+
+    while (i > 0)
+    {
+        int num = i % 10;
+        i = i / 10;
+        sum = sum + num;
+    }
+    
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма всех цифр в числе равна: " + FindSum(i));
+
+
+
