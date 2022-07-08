fecbaee8569a2055b6b3f35d96c66a058bad101b
Konstantin Ignatev <ignatyev.k@yandex.ru>
2022-07-08 14:31:32 +0300

    04.07.2022 homework done

--- 04.07.2022/Program.cs
+++ 04.07.2022/Program.cs
@@ -21,5 +21,35 @@ Console.WriteLine("Введите число");
 int i = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Сумма всех цифр в числе равна: " + FindSum(i));
 
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
 
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
 
+    for(int i=0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue +1);
+
+        return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateRandomArray(size, min, max);
+
+ShowArray(array);
