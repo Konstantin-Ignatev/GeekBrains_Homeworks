d86feaf46e5faeaa9b27367465d7c33731786bb9
Konstantin Ignatev <ignatyev.k@yandex.ru>
2022-07-22 23:27:33 +0300

    Seminar_09 added

---
+++ Seminar_09/Program.cs
@@ -0,0 +1,58 @@
+﻿void ShowNums(int n)
+{
+    if (n > 1)
+    {
+        Console.Write(n + " ");
+        ShowNums(n - 1);
+    }
+
+    Console.Write(n + " ");
+}
+
+
+
+int SumOfDigits(int n)
+{
+    if (n >= 10)
+    {
+        return n % 19 + SumOfDigits(n / 10);
+    }
+    return n;
+}
+
+ShowNums(5);
+Console.WriteLine();
+Console.WriteLine(SumOfDigits(12345));
+
+// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от M до N.
+
+void ShowNums(int m, int n)
+{
+    if (n > m)
+    {
+        Console.Write(" ");
+        ShowNums(m, n - 1);
+    }
+
+    Console.Write(n + " ");
+}
+
+ShowNums(6, 10);
+
+
+
+//Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+int ShowNums(int a, int b)
+{
+    if (b > 1)
+    {
+        return a * ShowNums(a, b - 1);
+    }
+
+    return a;
+}
+
+Console.WriteLine(ShowNums(2, 4));
