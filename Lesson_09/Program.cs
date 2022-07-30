e23fd189bbab71089986607151c4bf5a412e3ec9
Konstantin Ignatev <ignatyev.k@yandex.ru>
2022-07-30 22:13:35 +0300

    Lesson_09 edited

--- Lesson_09/Program.cs
+++ Lesson_09/Program.cs
@@ -23,7 +23,7 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"\nКол-во цифр в числе {numberLength(n)}: ");
-/*
+
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
@@ -46,4 +46,3 @@ int N = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 
 Sum(M, N, sum);
-*/
